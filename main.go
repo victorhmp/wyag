7d820e235841cf07a75700e1ed25345c5860cf98
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-08 16:02:59 -0300

    Add list of supported subcommands and parsing logic

--- main.go
+++ main.go
@@ -7,7 +7,21 @@ import (
 )
 
 func main() {
+	// addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	// catFileCmd := flag.NewFlagSet("cat-file", flag.ExitOnError)
+	// checkIgnoreCmd := flag.NewFlagSet("check-ignore", flag.ExitOnError)
+	// checkoutCmd := flag.NewFlagSet("checkout", flag.ExitOnError)
+	// commitCmd := flag.NewFlagSet("commit", flag.ExitOnError)
+	// hashObjectCmd := flag.NewFlagSet("hash-object", flag.ExitOnError)
 	initCmd := flag.NewFlagSet("init", flag.ExitOnError)
+	// logCmd := flag.NewFlagSet("log", flag.ExitOnError)
+	// lsFilesCmd := flag.NewFlagSet("ls-files", flag.ExitOnError)
+	// lsTreeCmd := flag.NewFlagSet("ls-tree", flag.ExitOnError)
+	// revParseCmd := flag.NewFlagSet("rev-parse", flag.ExitOnError)
+	// rmCmd := flag.NewFlagSet("rm", flag.ExitOnError)
+	// showRefCmd := flag.NewFlagSet("show-ref", flag.ExitOnError)
+	// statusCmd := flag.NewFlagSet("status", flag.ExitOnError)
+	// tagCmd := flag.NewFlagSet("tag", flag.ExitOnError)
 
 	if len(os.Args) < 2 {
 		fmt.Println("A subcommand is required")
